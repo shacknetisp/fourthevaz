d98322df684fb6253ae3047a21d8af4e0ab23a47
roy williams <bewe@here.there>
2014-12-08 14:46:44 -0500

    ISON

--- evazbot/modules/m_ison.py
+++ evazbot/modules/m_ison.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 from base import *
-#Doesn't work (yet)
-defaultsearch = "ghosts"
+import ast
+from urllib.request import urlopen
+import pprint
 
+url="http://redflare.ofthings.net"
 
 def msg(mp):
-    if mp.cmd("ison"):
-        args = mp.args("ison")
-        splitargs = args.split()
-        main.sendcmsg("ison:")
-        search = defaultsearch
-        if splitargs >= 1:
-            search = args
-        else:
-            main.sendcmsg("Invalid Arguments!")
-        return True
-    return False
-
-
-"""
-def help():
-  time.sleep(0.25)
-  main.sendcmsg(".ison name of player: Search play.redeclipse.net for <name>.")
-"""
+  if mp.cmd("ison"):
+    search = mp.args()
+    found = {}
+    with urlopen(url+"/reports") as u:
+      servers = ast.literal_eval(u.read().decode())
+      for server in servers.keys():
+        for name_p in servers[server]['playerNames']:
+          name = name_p['plain']
+          if name.lower().find(search.lower()) != -1:
+            if server not in found:
+              found[server]=list()
+            found[server].append(name)
+      for s in found:
+        out = servers[s]['description']+": "
+        for p in found[s]:
+          out+=p+", "
+        main.sendcmsg(out)
+    return True
+  return False
+  
+def showhelp():
+  main.sendcmsg(".ison name of player: Search "+url+" for <name>.")
