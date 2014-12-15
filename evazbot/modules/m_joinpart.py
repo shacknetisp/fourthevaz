e0b89b0216bf5989bb4bd209cad5fe5a6345786e
roy williams <bewe@here.there>
2014-12-15 11:57:36 -0500

    JoinPart

--- evazbot/modules/m_joinpart.py
+++ evazbot/modules/m_joinpart.py
@@ -9,12 +9,12 @@ def msg(mp):
     if mp.isjp('JOIN'):
         main.adminlist[mp.ircuser()] = ''
         main.whois(mp.ircuser())
-        main.sendamsg(cmd.getname(mp.ircuser()) + ' is now in '
-                      + main.getchannel() + '.')
+        #main.sendamsg(cmd.getname(mp.ircuser()) + ' is now in '
+        #             + main.getchannel() + '.')
     elif mp.isjp('PART'):
         main.adminlist[mp.ircuser()] = ''
-        main.sendamsg(cmd.getname(mp.ircuser()) + ' has departed '
-                      + main.getchannel() + '.')
+        #main.sendamsg(cmd.getname(mp.ircuser()) + ' has departed '
+         #             + main.getchannel() + '.')
     elif mp.isquit():
         main.adminlist[mp.ircuser()] = ''
-        main.sendamsg(cmd.getname(mp.ircuser()) + ' has left IRC.')
+        #main.sendamsg(cmd.getname(mp.ircuser()) + ' has left IRC.')
