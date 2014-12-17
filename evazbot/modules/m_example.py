0ff441538474c544cad886853f7d86902a3fa733
roy williams <bewe@here.there>
2014-12-16 19:06:52 -0500

    Better m_example.py

--- evazbot/modules/m_example.py
+++ evazbot/modules/m_example.py
@@ -3,10 +3,19 @@
 from base import *
 
 
+#The message-received event
 def msg(mp):
+    #if .example is received as a command
     if mp.cmd("example"):
+        #Send a message echoing the arguments.
         main.sendcmsg("Example's Arguments: " + mp.args())
+        #Command was received
+        return True
+    #No command received
+    return False
 
 
+#The ".help example" event
 def showhelp():
+    #Send a basic help message
     main.sendcmsg(".example <arguments>: Return the arguments.")
