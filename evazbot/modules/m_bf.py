be0610d52211f91c2ce260f6b88dead7cbca68df
roy williams <bewe@here.there>
2014-12-16 20:22:50 -0500

    bf flooding fixed

--- evazbot/modules/m_bf.py
+++ evazbot/modules/m_bf.py
@@ -9,7 +9,9 @@ def msg(mp):
     if mp.cmd('bf'):
         cmdinput = mp.args()
         cmdoutput = bfbackend.evaluate(cmdinput, intext)
-        main.sendcmsg(cmdoutput)
+        main.sendcmsg(
+            cmdoutput[0:min(len(cmdoutput), 255)]
+            .replace("\n", " ").replace("\r", " "))
         intext = ""
     if mp.cmd("bfinput"):
         intext = mp.args()
