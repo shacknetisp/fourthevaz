51a85e935465c9f160e1e417e2f900446829e93b
roy williams <bewe@here.there>
2014-12-19 15:21:20 -0500

    .help spam fix

--- evazbot/modules/m_help.py
+++ evazbot/modules/m_help.py
@@ -6,15 +6,18 @@ def msg(mp):
     if mp.cmd(c_net.helpcommand):
         args = mp.args()
         splitargs = args.split()
-        if len(splitargs) == 0:
-            main.sendcmsg("Use .help <module> for a specific module.")
-            main.sendcmsg(
-                "http://ghostclanre.tk/phpbb3/viewtopic.php?f=16&t=21")
-            cmd.outlist(c_modules.helpmodulenames())
-        elif len(splitargs) == 1:
-            c_modules.showhelp(splitargs[0])
+        if mp.iswlist():
+            if len(splitargs) == 0:
+                main.sendcmsg("Use .help <module> for a specific module.")
+                main.sendcmsg(
+                    "http://ghostclanre.tk/phpbb3/viewtopic.php?f=16&t=21")
+                cmd.outlist(c_modules.helpmodulenames())
+            elif len(splitargs) == 1:
+                c_modules.showhelp(splitargs[0])
+            else:
+                main.sendcmsg("Invalid Arguments")
         else:
-            main.sendcmsg("Invalid Arguments")
+            main.sendcmsg("No access, contact Beha. ##4etesting on freenode.")
         return True
     return False
 
