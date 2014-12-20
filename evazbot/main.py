3ab2df3134e9d72094c5b02047166a4c69a157c1
roy williams <bewe@here.there>
2014-12-20 00:37:04 -0500

    use epoll on linux

--- evazbot/main.py
+++ evazbot/main.py
@@ -146,7 +146,38 @@ def process(ircmsgp):
             c_modules.event("afterall", ircmsg)
 
 
-def loop():
+def loop_poll():
+    import evazbot.configs.c_modules as c_modules
+    global channel
+    global currentprofile
+    lasttime = time.time()
+    while running:
+        poll = select.epoll()
+        for p in range(len(ircprofiles)):
+            poll.register(ircprofiles[p]["ircsock"], select.POLLIN)
+        ready = poll.poll(0.2)
+        time.sleep(0.1)
+        if time.time() - lasttime >= 1:
+            lasttime = time.time()
+            c_modules.event("tick")
+        for fd, flag in ready:
+            for r in range(len(ircprofiles)):
+                if ircprofiles[r]["ircsock"].fileno() == fd:
+                    currentprofile = r
+                try:
+                    ircmsg = ircprofiles[currentprofile][
+                    "ircsock"].recv(2048).decode()
+                    regex = re.compile(
+                    "\x03(?:\d{1,2}(?:,\d{1,2})?)?", re.UNICODE)
+                    ircmsg = regex.sub("", ircmsg)
+                except socket.error as exc:
+                    raise exc
+                msgs = ircmsg.split("\r\n")
+                for i in msgs:
+                    process(i)
+
+
+def loop_select():
     import evazbot.configs.c_modules as c_modules
     global channel
     global currentprofile
@@ -155,7 +186,7 @@ def loop():
         sockets = []
         for p in range(len(ircprofiles)):
             sockets.append(ircprofiles[p]["ircsock"])
-        selectresult, sr2, sr3 = select.select(sockets, [], [], 1)
+        selectresult, sr2, sr3 = select.select(sockets, [], [], 0.2)
         if time.time() - lasttime >= 1:
             lasttime = time.time()
             c_modules.event("tick")
@@ -182,7 +213,12 @@ def ircmain():
     c_modules.load()
     print(("Starting " + c_net.name + "..."))
     c_modules.event("start")
-    loop()
+    if hasattr(select, "epoll"):
+        print("epoll()")
+        loop_poll()
+    else:
+        print("select()")
+        loop_select()
     c_modules.event("stop")
     for i in range(len(ircprofiles)):
         ircprofiles[i]["ircsock"].close()
