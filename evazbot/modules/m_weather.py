e7a1dc3fa63f3bd2d46da34a034ccd3eee85199f
roy williams <bewe@here.there>
2014-12-20 15:57:32 -0500

    more stuff, help text

--- evazbot/modules/m_weather.py
+++ evazbot/modules/m_weather.py
@@ -14,20 +14,20 @@ class weatherinfo:
         if number == "currenttemp":
             if self.outputtemp == "kel":
                 return "It is " +\
-                str(data["main"]["temp"]) + " in " + data["name"]
+                str(data["main"]["temp"]) + " kel in " + data["name"]
             elif self.outputtemp == "cel":
                 temp = Decimal(str(data["main"]["temp"])) - Decimal('273.15')
-                return "It is " + str(temp) + " in " + data["name"]
+                return "It is " + str(temp) + " cel in " + data["name"]
             elif self.outputtemp == "far":
                 temp = Decimal(str(data["main"]["temp"])) - Decimal('273.15')
                 temp = Decimal(str(temp)) * Decimal('1.8') + Decimal('32.0')
-                return "It is " + str(temp) + " in " + data["name"]
+                return "It is " + str(temp) + " far in " + data["name"]
             else:
                 raise ValueError(
                     "Invalid Temperature Style: " + self.outputtemp)
         elif number == "currentwindspeed":
             return "The Wind is Blowing at " +\
-            str(data["wind"]["speed"]) + " m/s"
+            str(data["wind"]["speed"]) + " m/s in " + data["name"]
 
     def __init__(self, url, style):
         jsonobj = urlopen(url)
@@ -75,7 +75,7 @@ def msg(mp):
             elif data["cod"] == "404":
                 main.sendcmsg(data["cod"] + ":" + data["message"])
 
-
+'''
 def showhelp():
     main.sendcmsg(
         'Weather is a module that returns many usefull weather information.'
@@ -90,3 +90,14 @@ def showhelp():
     "Please use the closest large town")
     main.sendcmsg("        -id: Looks up by city id")
     main.sendcmsg("        -wind: Finds wind information")
+'''
+
+
+def showhelp():
+    main.sendcmsg(
+        ".weather [-temp] [-wind] [-cel -kel -far] [-name -id]" +
+        "<city name/id>:" +
+        " Get weather from http://openweathermap.org")
+    main.sendcmsg("-temp, -wind: Get temperature, wind.")
+    main.sendcmsg("-cel, -kel, -far: Use celsius, kelvin, farenheit.")
+    main.sendcmsg("-name, id: Use city name or ID, -name is default.")
