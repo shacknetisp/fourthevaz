54a3ebf71cf9b2a04f5885f3294974ba58319372
roy williams <bewe@here.there>
2015-01-10 19:54:15 -0500

    fix

--- evazbot/default/m_joinpart.py
+++ evazbot/default/m_joinpart.py
@@ -2,27 +2,15 @@
 
 from base import *
 
-announce_entries = False
-##mconfig/joinpart.py
-#announce_entries = True #show message for joining, parting, and quitting
-exec(c_locs.mconfig('joinpart'))
-
 
 def msg(mp):
     if mp.isserver():
         return
-    if mp.isjp('JOIN'):
+    if mp.isjp('JOIN') or mp.code('JOIN'):
         main.ircprofiles[main.currentprofile]["adminlist"][mp.ircuser()] = ''
         main.whois(mp.ircuser())
-        if announce_entries:
-            main.sendamsg(cmd.getname(mp.ircuser()) + ' is now in '
-                     + main.getchannel() + '.')
-    elif mp.isjp('PART'):
+    elif mp.isjp('PART') or mp.code('PART'):
         main.ircprofiles[main.currentprofile]["adminlist"][mp.ircuser()] = ''
-        if announce_entries:
-            main.sendamsg(cmd.getname(mp.ircuser()) + ' has departed '
-                      + main.getchannel() + '.')
-    elif mp.isquit():
+        main.whois(mp.ircuser())
+    elif mp.isquit() or mp.code('QUIT'):
         main.ircprofiles[main.currentprofile]["adminlist"][mp.ircuser()] = ''
-        if announce_entries:
-            main.sendamsg(cmd.getname(mp.ircuser()) + ' has left IRC.')
