d04cfc6bca6eb603764af3d13bf667e2dc47e954
roy williams <bewe@here.there>
2015-01-16 00:12:49 -0500

    improved and useful m_timec

--- evazbot/default/m_timec.py
+++ evazbot/default/m_timec.py
@@ -1,36 +1,85 @@
 # -*- coding: utf-8 -*-
 from base import *
-from datetime import datetime
+import pytz
+from datetime import datetime, tzinfo, timedelta
+import random
 
 
-def tuntil(mp, f):
-    tdelta = datetime.utcnow() \
-        - datetime.strptime(mp.argsdef().strip(), f + '/%y %H:%M')
-    seconds = (tdelta.microseconds + (tdelta.seconds + tdelta.days * 24
-               * 3600) * 10 ** 6) / 10 ** 6
-    main.sendcmsg(str(round(abs(seconds / 3600), 2)) + ' hours')
+ZERO = timedelta(0)
 
 
-def msg(mp):
-    if mp.cmd('time utc'):
-        main.sendcmsg(datetime.utcnow().strftime('%H:%M'))
-        return True
-    if mp.cmd('time until'):
-        try:
-            if mp.argbool('dmy'):
-                tuntil(mp, '%d/%m')
-            else:
-                tuntil(mp, '%m/%d')
-        except ValueError:
-            main.sendcmsg('Bad time format, possibly a number is wrong. " +\
-            "(00:00, not 24:00?)'
-                          )
-        return True
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return ZERO
+
+
+def gettimezones(search, maxi):
+    results = []
+    for i in pytz.all_timezones:
+        if i.lower().find(search.lower()) != -1 or\
+        c_regex.casecontains(search, i):
+            if len(results) < maxi or maxi == 0:
+                results.append(i)
+    return results
+
+
+def gettimezone(search):
+    for i in pytz.all_timezones:
+        if i.lower() == search.lower():
+            return i
+    return ""
+
+
+def get(ct):
+    if ct.cmd('time'):
+        if ct.args.getbool('getzone'):
+            results = ['Results']
+            results += gettimezones(ct.args.getdef(), 13)
+            cmd.outlist(results)
+            return True
+        else:
+            try:
+                target = pytz.timezone(gettimezone(ct.args.getdef().upper()))
+            except pytz.exceptions.UnknownTimeZoneError:
+                try:
+                    ct.msg('Cannot find timezone, do you mean %s?' % (str(
+                        random.choice(gettimezones(ct.args.getdef(), 0)))))
+                except IndexError:
+                    ct.msg('Cannot find timezone, try ' +
+                    cmd.cprefix() + 'time -getzone <search>')
+                return True
+            t = datetime.now(UTC())
+            utcs = '%d:%d' % (t.hour, t.minute)
+            try:
+                t = t.replace(hour=int(ct.args.get(
+                    'utc', utcs).split(':')[0]), minute=int(ct.args.get(
+                        'utc', utcs).split(':')[1]))
+                utcs = '%02d:%02d' % (t.hour, t.minute)
+                utcs = 'UTC: ' + utcs
+            except IndexError:
+                main.sendcmsg('Invalid -utc format.')
+                return
+            except ValueError:
+                main.sendcmsg('Invalid -utc format.')
+                return
+            t = t.astimezone(target)
+            utcs += ' is %s: %02d:%02d' % (
+                ct.args.getdef().upper(), t.hour, t.minute)
+            ct.msg(utcs)
+            return True
     return False
 
 
-def showhelp():
-    main.sendcmsg(cmd.cprefix() + 'time utc: Get current UTC time.')
-    main.sendcmsg(cmd.cprefix() + 'time until [-dmy] <d/m/y H:M>:" +\
-    "Get hours and minutes until <date> utc. Default is -mdy.'
-                  )
+def showhelp(h):
+    h("time [-utc=<00:00>] <timezone/region>: " +
+    "get current time, or UTC time from -utc")
+    h("time -getzone <zonesearch>: " +
+    "Get first 12 timezones matching <zonesearch>.")
