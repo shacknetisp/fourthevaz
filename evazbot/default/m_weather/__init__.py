351f556d2c5d5203b137ca0c8c2d7130ba2b099c
acz13 <alchzh@gmail.com>
2015-01-26 20:14:44 -0500

    Forecast Rough Draft! Readme FIx

--- evazbot/default/m_weather/__init__.py
+++ evazbot/default/m_weather/__init__.py
@@ -1,5 +1,7 @@
 from base import *
 import pygeoip
+import requests
+from xmltodict import parse
 weather = mload('m_weather.weather')
 gi = pygeoip.GeoIP('deps/pygeoip/GeoLiteCity.dat')
 
@@ -36,6 +38,44 @@ def get(ct):
             if thismsg != lastmsg:
                 ct.msg(thismsg)
             lastmsg = thismsg
+'''    elif ct.cmd == 'forecast':
+        opt = {}
+        if (not ct.args.getbool('id')) (and not ct.args.getbool('name')) and (not ct.args.getbool('geoip')):
+            ct.msg('Specify an input method!')
+        elif ct.args.getbool('id')
+            opt['id'] = ct.args.getdef()
+        elif ct.args.getbool('geoip'):
+            ip = ct.args.getdef()
+            urlid = False
+            #urlcity = True
+            try:
+                r = gi.record_by_addr(ip)
+            except:
+                    r = gi.record_by_name(ip)
+                except:
+                    r = None
+            try:
+                opt['q'] = r['city'] + ', ' + r['region_code'] + ', '\
+                        + r['country_code']
+            except TypeError:
+                ct.msg('Cannot get GeoIP information.')
+                break
+        try:
+            opt['cnt'] = ct.args.get('days')
+        except ValueError:
+            opt['cnt'] = '7'
+
+        url = 'api.openweathermap.org/data/2.5/forecast?'
+        r = requests.get(url, params = opt)
+        data = r.parse()
+        data = data['weatherdata']['forecast']['time']
+        info = []
+        if ct.args.getbool('windSpeed'):
+            if ct.args.get('windSpeed') in data['windSpeed']: '''
+
+
+
+
 
 
 def showhelp():
