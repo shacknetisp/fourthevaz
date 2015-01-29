2d1d3b522d69b8ffea9445a6ba49ffcd2d913142
roy williams <bewe@here.there>
2015-01-28 21:41:24 -0500

    if at first you don't succed, try try again

--- deps/iso639/__init__.py
+++ deps/iso639/__init__.py
@@ -5,21 +5,23 @@ import codecs
 if not 'unicode' in dir():
     unicode = str
 
+
 class NonExistentLanguageError(RuntimeError):
     pass
 
+
 def find(whatever=None, language=None, iso639_1=None, iso639_2=None):
     if whatever:
-        keys = [u'name', u'iso639_1', u'iso639_2_b', u'iso639_2_t']
+        keys = ['name', 'iso639_1', 'iso639_2_b', 'iso639_2_t']
         val = whatever
     elif language:
-        keys = [u'name']
+        keys = ['name']
         val = language
     elif iso639_1:
-        keys = [u'iso639_1']
+        keys = ['iso639_1']
         val = iso639_1
     elif iso639_2:
-        keys = [u'iso639_2_b', u'iso639_2_t']
+        keys = ['iso639_2_b', 'iso639_2_t']
         val = iso639_2
     else:
         raise ValueError('Invalid search criteria.')
@@ -44,7 +46,7 @@ def to_iso639_1(key):
     item = find(whatever=key)
     if not item:
         raise NonExistentLanguageError('Language does not exist.')
-    return item[u'iso639_1']
+    return item['iso639_1']
 
 
 def to_iso639_2(key, type='B'):
@@ -54,8 +56,8 @@ def to_iso639_2(key, type='B'):
     if not item:
         raise NonExistentLanguageError('Language does not exist.')
     if type == 'T' and item[u'iso639_2_t']:
-        return item[u'iso639_2_t']
-    return item[u'iso639_2_b']
+        return item['iso639_2_t']
+    return item['iso639_2_b']
 
 
 def to_name(key):
@@ -69,15 +71,16 @@ def _load_data():
     def parse_line(line):
         data = line.strip().split('|')
         return {
-            u'iso639_2_b': data[0],
-            u'iso639_2_t': data[1],
-            u'iso639_1': data[2],
-            u'name': data[3].split(';')[0],
+            'iso639_2_b': data[0],
+            'iso639_2_t': data[1],
+            'iso639_1': data[2],
+            'name': data[3].split(';')[0],
         }
 
-    data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ISO-639-2_utf-8.txt')
+    data_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'ISO-639-2_utf-8.txt')
     with codecs.open(data_file, 'r', 'UTF-8') as f:
-        data = [ parse_line(line) for line in f]
+        data = [parse_line(line) for line in f]
     return data
 
 data = _load_data()
