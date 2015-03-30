9f59f10b0f910cf5764779ea08f4c570512323b3
bewe <bewe@here.there>
2015-03-30 18:20:40 -0400

    use running for dbs instead of the text.py file itself

--- db/text.py
+++ db/text.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import ast
 import pprint
-dbs = {}
+import running
 
 
 class DB:
 
     def __init__(self, filename, db=dict()):
         self.filename = filename
-        if self.filename not in dbs:
-            dbs[self.filename] = db
+        if self.filename not in running.dbs:
+            running.dbs[self.filename] = db
 
     def db(self):
-        return dbs[self.filename]
+        return running.dbs[self.filename]
 
     def load(self):
-        dbs[self.filename] = ast.literal_eval(open(self.filename).read())
+        running.dbs[self.filename] = ast.literal_eval(
+            open(self.filename).read())
 
     def save(self):
-        open(self.filename, 'w').write(pprint.pformat(dbs[self.filename]))
+        open(self.filename, 'w').write(
+            pprint.pformat(running.dbs[self.filename]))
--- running.py
+++ running.py
@@ -2,3 +2,4 @@
 working_servers = []
 accesslist = None
 serverdb = None
+dbs = {}
