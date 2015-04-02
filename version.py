ef9097d0cb27c379c69c5c496efb0705b1e0fc7a
bewe <bewe@here.there>
2015-04-02 15:53:12 -0400

    add git versioning

--- version.py
+++ version.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 import platform
 name = "Fourth Evaz"
-version = (0, 1, 6)
+version = (0, 1, 7)
 source = "https://github.com/shacknetisp/fourthevaz"
 
 
 def versionstr():
-    return "%d.%d.%d" % (version[0], version[1], version[2])
+    gitstr = ""
+    try:
+        gitstr = "-%s" % (open('.git/refs/heads/master').read().strip()[0:10])
+    except FileNotFoundError:
+        pass
+    return "%d.%d.%d%s" % (version[0], version[1], version[2], gitstr)
 
 
 def pythonversionstr():
