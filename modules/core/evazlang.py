6b4db2c81f0d230da60be8822133856052830e07
bewe <bewe@here.there>
2015-04-08 09:37:22 -0400

    <, >, and == math functions; wordcount, or, and, and if logic functions

---
+++ modules/core/evazlang.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import configs.module
+import utils
+from . import commands
+
+
+def init():
+    m = configs.module.Module(__name__)
+    m.set_help('Various string and parser functions.')
+    #Strings
+    m.add_short_command_hook(
+        wordcount,
+        'wordcount::Get a count of words.',
+        ['words...::Words to count.'])
+    m.add_command_alias('wc', 'wordcount')
+    #Logic
+    m.add_short_command_hook(
+        if_f,
+        'if::If <value> then <then> else <else>.',
+        ['value::Value to test.',
+        'then::Command to execute if value.',
+        'else::Command to execute if not value.',
+        ])
+    m.add_short_command_hook(
+        or_f,
+        'or::Return if <a> or <b>',
+        [
+            'a::Value to test',
+            'b::Value to test',
+            ]
+        )
+    m.add_short_command_hook(
+        and_f,
+        'and::Return if <a> and <b>',
+        [
+            'a::Value to test',
+            'b::Value to test',
+            ]
+        )
+    return m
+
+
+def or_f(fp, args):
+    try:
+        return str(utils.boolstr(args.getlinstr('a')) or utils.boolstr(
+            args.getlinstr('b')))
+    except ValueError:
+        return 'Invalid truth value.'
+
+
+def and_f(fp, args):
+    try:
+        return str(utils.boolstr(args.getlinstr('a')) and utils.boolstr(
+            args.getlinstr('b')))
+    except ValueError:
+        return 'Invalid truth value.'
+
+
+def wordcount(fp, args):
+    return str(len(args.getlinstr('words', '').split()))
+
+
+def if_f(fp, args):
+    try:
+        if utils.boolstr(args.getlinstr('value')):
+            return commands.doptext(fp, args.getlinstr('then'))
+        else:
+            return commands.doptext(fp, args.getlinstr('else'))
+    except ValueError:
+        return 'Invalid truth value.'
--- modules/default/calc/__init__.py
+++ modules/default/calc/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import safeeval
 import importlib
+import utils
 importlib.reload(safeeval)
-# -*- coding: utf-8 -*-
 from configs.module import Module
 
 
@@ -15,6 +15,12 @@ def init():
             'help': 'Calculate an expression (Python Syntax).',
             'args': [
                 {
+                    'name': 'truth',
+                    'optional': True,
+                    'keyvalue': '',
+                    'help': 'Output as a truth value.',
+                    },
+                {
                     'name': 'expr',
                     'optional': False,
                     'end': True,
@@ -22,13 +28,51 @@ def init():
                     },
                 ],
             })
+    #Math
+    m.add_short_command_hook(
+        lt,
+        'lt::Return if <a> < <b>.',
+        ['a::Value to test.',
+        'b::Value to test.'])
+    m.add_short_command_hook(
+        gt,
+        'gt::Return if <a> > <b>.',
+        ['a::Value to test.',
+        'b::Value to test.'])
+    m.add_short_command_hook(
+        eq,
+        'eq::Return if <a> == <b>.',
+        ['a::Value to test.',
+        'b::Value to test.'])
     return m
+
+
+def lt(fp, args):
+    try:
+        return str(float(args.getlinstr('a')) < float(args.getlinstr('b')))
+    except ValueError:
+        return 'Not numbers!'
+
+
+def gt(fp, args):
+    try:
+        return str(float(args.getlinstr('a')) > float(args.getlinstr('b')))
+    except ValueError:
+        return 'Not numbers!'
+
+
+def eq(fp, args):
+    try:
+        return str(float(args.getlinstr('a')) == float(args.getlinstr('b')))
+    except ValueError:
+        return 'Not numbers!'
 
 
 def calc(fp, args):
     arg = args.getlinstr('expr')
     try:
-        return('(%s) = (%s)' % (
-        arg, str(float(safeeval.domath(arg)))))
+        if 'truth' in args.lin:
+            return str(bool(round(safeeval.domath(arg))))
+        return('%s' % (str(float(safeeval.domath(arg)))))
     except Exception as e:
         return('Error: ' + str(e))
