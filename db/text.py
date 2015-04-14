fb7b18a220571395fc2bc55a37064a54144ccc8d
Beha <bewe@here.there>
2015-04-14 13:15:00 -0400

    db.text docstrings

--- db/text.py
+++ db/text.py
@@ -6,20 +6,25 @@ import os
 
 
 class DB:
+    """A text database object, saves in pprint format."""
 
     def __init__(self, filename, db=dict()):
+        """Register a DB object with <filename> and initialize with <db>."""
         self.filename = filename
         if self.filename not in running.dbs:
             running.dbs[self.filename] = db
 
     def db(self):
+        """Get the db."""
         return running.dbs[self.filename]
 
     def load(self):
+        """Load the db."""
         running.dbs[self.filename] = ast.literal_eval(
             open(self.filename).read())
 
     def save(self):
+        """Safe-save the db."""
         with open(self.filename + '.working', 'w') as f:
             f.write(pprint.pformat(running.dbs[self.filename]))
         try:
