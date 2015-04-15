8dfada9fe8f12db2090a939d0c9b43bc74a59d4e
Beha <bewe@here.there>
2015-04-15 15:30:38 -0400

    remove linecount

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 if [[ $@ == **upload** ]]
 then
-( find . -name '*' -not -path "./.git/*" -not -type d -not -path "./userdata/*" -print0 | xargs -0 cat ) | wc -l > linecount
+#( find . -name '*' -not -path "./.git/*" -not -type d -not -path "./userdata/*" -print0 | xargs -0 cat ) | wc -l > linecount
 git add --all -v .
 git commit
 git push
