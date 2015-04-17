ba7403c49d85ec3ea94fdef812e84061ae1f269c
Beha <bewe@here.there>
2015-04-16 23:00:14 -0400

    some more flexibility

--- modules/default/linkparser.py
+++ modules/default/linkparser.py
@@ -9,15 +9,35 @@ URL_REGEX = r"""
 \b
 """
 import re
+linkparse = None
 
 
-def init():
+def init(options):
+    global linkparse
+    linkparse = options['server'].import_module('share.linkparse', True)
     m = configs.module.Module(__name__)
     m.set_help('Preform operations on links.')
     m.add_short_command_hook(title,
         'title::Get the title of a URL.', ['[url]::Url to view.'])
     m.add_base_hook('recv', recv)
     return m
+
+
+def gettitle(url):
+    title = ""
+    try:
+        try:
+            title = linkparse.get_title(url, 2)
+        except requests.exceptions.MissingSchema:
+            url = 'http://' + url
+            title = linkparse.get_title(url, 2)
+    except requests.exceptions.ConnectionError:
+        title = ""
+    except socket.timeout:
+        title = ""
+    except requests.exceptions.Timeout:
+        title = ""
+    return title
 
 
 def recv(fp):
@@ -36,18 +56,5 @@ def title(fp, args):
         fp.server.state['lastlink.%s' % fp.outtarget()]
         if 'lastlink.%s' % fp.outtarget() in fp.server.state else
         None)
-    try:
-        try:
-            title = fp.server.import_module(
-                'share.linkparse', False).get_title(url, 2)
-        except requests.exceptions.MissingSchema:
-            url = 'http://' + url
-            title = fp.server.import_module(
-                'share.linkparse', False).get_title(url, 2)
-    except requests.exceptions.ConnectionError:
-        title = ""
-    except socket.timeout:
-        title = ""
-    except requests.exceptions.Timeout:
-        title = ""
+    title = gettitle(url)
     return title if title else 'Cannot find the title for %s.' % url
