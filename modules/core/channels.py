1a69293a4f9c52532e7c0825af8a0c0c31241310
Beha <bewe@here.there>
2015-04-17 22:38:48 -0400

    better channel handling

--- modules/core/channels.py
+++ modules/core/channels.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from configs.module import Module
+import bot
+import utils
 
 
 def init():
@@ -31,7 +33,20 @@ def init():
                     },
                 ],
             })
+    m.add_timer_hook(10 * 1000, timer)
     return m
+
+
+def timer():
+    for server in bot.servers():
+        if server.nick in server.whoislist:
+            if 'channels' in server.whoislist[server.nick]:
+                todi = []
+                for i in range(len(server.channels)):
+                    c = server.channels[i]
+                    if c not in server.whoislist[server.nick]['channels']:
+                        todi.append(i)
+                server.channels = utils.remove_indices(server.channels, todi)
 
 
 def join(fp, args):
@@ -41,11 +56,11 @@ def join(fp, args):
         if c['channel'] == channel:
             fp.server.write_cmd('PART', fp.server.shortchannel(c)['channel'])
             fp.server.write_cmd('JOIN', fp.server.shortchannel(c)['channel'])
-            return "Re-joined %s" % channel
+            return "Attempted to rejoin %s" % channel
 
     fp.server.channels.append(fp.server.shortchannel(channel))
     fp.server.join_channel(channel)
-    return "Joined %s" % channel
+    return "Attempted to join %s" % channel
 
 
 def part(fp, args):
@@ -61,5 +76,6 @@ def part(fp, args):
             fp.server.write_cmd('PART', fp.server.shortchannel(c)['channel'])
             if ch:
                 fp.replypriv('Parted %s' % channel)
+                return
             else:
                 return 'Parted %s' % channel
