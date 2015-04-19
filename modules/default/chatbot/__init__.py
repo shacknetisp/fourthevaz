f5f11da9005ba4ffaba2e611ba1b2f08c1f41162
Beha <bewe@here.there>
2015-04-19 16:28:30 -0400

    move from wordai to ailib

--- modules/default/chatbot/__init__.py
+++ modules/default/chatbot/__init__.py
@@ -1,58 +1,35 @@
 # -*- coding: utf-8 -*-
 import configs.module
 import configs.locs
-from . import wordai
+from . import ailib
+import time
 
 
 def init(options):
     server = options['server']
-    server.state['wordai'] = wordai.wordai(
-        configs.locs.userdb + '/chatbot.%s.pkl' % server.entry['settings'])
-    server.state['wordai'].load()
+    server.state['ai'] = ailib.AI(
+        configs.locs.userdb + '/ailib.%s.py' % server.entry['settings'])
     m = configs.module.Module(__name__)
     m.set_help('Talk with the chatbot.')
-    m.add_command_hook('chatbot',
-        {
-            'function': chatbot,
-            'rights': ['normal'],
-            'help': 'Talk to the bot.',
-            'noquote': True,
-            'args': [
-                {
-                    'name': 'fix',
-                    'help': 'Change the first word of arg to the second word.',
-                    'keyvalue': '',
-                    'optional': True,
-                    },
-                {
-                    'name': 'count',
-                    'help': 'Get the word count.',
-                    'keyvalue': '',
-                    'optional': True,
-                    },
-                {
-                    'name': 'args',
-                    'help': 'Arguments or message to send.',
-                    'optional': False,
-                    'end': True,
-                    },
-                ]
-            })
+    m.add_short_command_hook(chatbot, 'chatbot::Talk to the bot',
+        ['text...::What you have to say.'],
+        'normal')
     m.add_command_alias('c', 'chatbot')
     m.add_command_alias('talk', 'chatbot')
     return m
 
 
 def chatbot(fp, args):
-    wordai = fp.server.state['wordai']
-    if 'count' in args.lin:
-        return str(wordai.getwords())
-    elif 'fix' in args.lin:
-        try:
-            f, t = args.getlinstr('args').split()
-        except IndexError:
-            return 'Usage: -fix <from> <to>.'
-        wordai.replace(f, t)
-        return '"%s" is now "%s".' % (f, t)
-    else:
-        return wordai.process(args.getlinstr('args'))
+    ai = fp.server.state['ai']
+    e = 'chatbot.lastphrase.%s' % fp.outtarget()
+    etime = e + '.time'
+    lasttime = fp.server.state[
+        etime] if etime in fp.server.state else time.time()
+    lastphrase = fp.server.state[e] if e in fp.server.state else None
+    if (time.time() - lasttime) > 30:
+        lastphrase = ""
+    fp.server.state[
+        etime] = time.time()
+    out = ai.process(args.getlinstr('text'), lastphrase)
+    fp.server.state[e] = out
+    return out
---
+++ modules/default/chatbot/ailib.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import ast
+import pprint
+import os
+import random
+import string
+
+
+def intorrand(t):
+    if type(t) is not int:
+        return random.randrange(t[0], t[1])
+    else:
+        return t
+
+
+class AI:
+
+    def __init__(self, path):
+        self.path = path
+        self.db = {}
+        self.lastphrase = None
+        if not os.path.exists(path):
+            self.save()
+        self.load()
+
+        def makeentry(n, t={}):
+            if n not in self.db or type(self.db[n]) is not type(t):
+                self.db[n] = t
+        makeentry('phrases', [])
+
+    def load(self):
+        self.db = ast.literal_eval(open(self.path).read())
+
+    def save(self):
+        open(self.path, 'w').write(pprint.pformat(self.db))
+
+    def keywords(self, text):
+        ret = []
+        for word in text.split():
+            ret.append(word)
+        return ret
+
+    def process(self, text, lastphrase=None):
+        if lastphrase is not None:
+            self.lastphrase = lastphrase
+        for c in string.punctuation:
+            text.replace(c, '')
+        keywords = self.keywords(text)
+
+        def diff(t):
+            a = 0
+            for kw in keywords:
+                if kw in t:
+                    a += 1
+            return (a / len(t))
+        if self.lastphrase:
+            x = (self.lastphrase, text, self.keywords(self.lastphrase))
+            if x not in self.db['phrases']:
+                self.db['phrases'].append(x)
+        self.db['phrases'].append([text])
+        possible = list(filter(lambda x: len(x) == 3, self.db['phrases']))
+        random.shuffle(possible)
+        for i in possible:
+            if i[0].lower() == text.lower():
+                self.lastphrase = i[1]
+                return i[1]
+        try:
+            out = list(reversed(sorted(possible, key=lambda x: diff(x[2]))))[
+                0:max(min(round(len(possible) / 7),
+                random.randrange(1, 5)), 1)]
+            out = random.choice(out)
+            self.lastphrase = out[1]
+            return out[1]
+        except IndexError:
+            pass
+        out = random.choice(
+            list(filter(lambda x: len(x) == 1, self.db['phrases'])))
+        self.lastphrase = out[0]
+        return out[0]
