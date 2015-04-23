d3f7c6f1bf5d2afcbc1084668133648426d0b596
Blank <shacknetisp@hotmail.com>
2015-04-23 17:52:04 -0400

    better redflare modhelp

--- modules/default/redflare/__init__.py
+++ modules/default/redflare/__init__.py
@@ -25,7 +25,7 @@ def init(options):
     options['server'].state['redflare'].save()
 
     m = configs.module.Module(__name__)
-    m.set_help('Operate on RedFlares.')
+    m.set_help('Get server information from Redflares.')
     m.add_command_hook('redflare', {
         'function': doredflare,
         'help': 'Search players on a RedFlare.',
