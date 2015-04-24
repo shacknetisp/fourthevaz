cb4d5ebe9c005bc881a08d36f99348eeeef84f0e
Blank <shacknetisp@hotmail.com>
2015-04-24 18:54:29 -0400

    attempt to work better

--- modules/core/api.py
+++ modules/core/api.py
@@ -15,9 +15,15 @@ def init(options):
         del options['server'].state['wserver']
     try:
         if 'apiport' in options['server'].entry:
-            options['server'].state[
+            try:
+                options['server'].state[
+                        'wserver'] = wsgiref.simple_server.make_server(
+                    '', options['server'].entry['apiport'],
+                    application(options['server']))
+            except TypeError:
+                options['server'].state[
                     'wserver'] = wsgiref.simple_server.make_server(
-                '', options['server'].entry['apiport'],
+                0, options['server'].entry['apiport'],
                 application(options['server']))
             print(('Opening API server on %d' % options[
                 'server'].entry['apiport']))
